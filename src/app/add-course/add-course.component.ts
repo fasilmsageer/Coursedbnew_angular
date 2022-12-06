7bb4922057e68cf2241765e70162f1f9604b7702
fasilmsageer <fasilmsageer@gmail.com>
2022-12-06 20:49:58 +0530

    apiservice

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-course',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-course.component.css']
 })
 export class AddCourseComponent {
+
+  coursetitle=""
+  coursedescription=""
+  courseduration=""
+  coursedate=""
+  coursevenue=""
+
+  constructor(private api:ApiService){}
+
+
+  readValue=()=>
+  {
+    let data:any={"coursetitle":this.coursetitle,"coursedescription":this.coursedescription,"courseduration":this.courseduration,"coursedate":this.coursedate,"coursevenue":this.coursevenue}
+    console.log(data)
+    
+    this.api.addCourses(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        if (response.status=="success"){
+          this.coursetitle=""
+          this.coursedescription=""
+          this.courseduration=""
+          this.coursevenue=""
+          this.coursedate=""
+        } else{
+          alert("something went wrong")
+
+        }
+        
+      }
+    )
+  }
+
 
 }
